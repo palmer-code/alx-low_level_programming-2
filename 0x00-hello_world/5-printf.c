bdef0c3f9fbdfb22f48f24a85372b20ba7d7d842
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-02-16 04:32:49 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main: Print a string
+ * Description: Use printf
+ * Return )
+ */
+
+int main(void)
+{
+	printf("%s","with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
