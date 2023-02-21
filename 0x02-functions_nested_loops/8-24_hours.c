f099af93b8dc0015c6a459f904feb038402404a7
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-02-21 07:54:19 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *jack_bauer - prints every minute of the day
+ * Description: prints every minute of the day
+ * Return: void
+ */
+
+void jack_bauer(void) 
+{
+    int hour, minute;
+
+    for (hour = 0; hour < 24; hour++) 
+    {
+        for (minute = 0; minute < 60; minute++) 
+	{
+            char hour_tens = '0' + (hour / 10);
+            char hour_units = '0' + (hour % 10);
+            char minute_tens = '0' + (minute / 10);
+            char minute_units = '0' + (minute % 10);
+
+           _putchar(hour_tens);
+           _putchar(hour_units);
+           _putchar(':');
+           _putchar(minute_tens);
+           _putchar(minute_units);
+           _putchar('\n');
+        }
+    }
+}
+
