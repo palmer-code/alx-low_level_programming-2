4c09934d9783a5a903ff9c19050184efa8b74e31
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-02-21 10:04:40 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,24 +5,42 @@
  * Description: prints every minute of the day
  * Return: void
  */
-
-void jack_bauer(void) 
+void jack_bauer(void)
 {
-	int hour, minute;
-	for (hour = 0; hour < 24; hour++) 
-    {
-        for (minute = 0; minute < 60; minute++) 
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
+
+	hours_max = 58;
+	hours_tens = '0';
+	while (hours_tens < '3')
 	{
-            char hour_tens = '0' + (hour / 10);
-            char hour_units = '0' + (hour % 10);
-            char minute_tens = '0' + (minute / 10);
-            char minute_units = '0' + (minute % 10);
-	    	_putchar(hour_tens);
-		_putchar(hour_units);
-		_putchar(':');
-	       	_putchar(minute_tens);
-		_putchar(minute_units);
-		_putchar('\n');
-        }
-    }
+		if (hours_tens == '2')
+		{
+			hours_max = '4';
+		}
+		hours_ones = '0';
+		while (hours_ones < hours_max)
+		{
+			minutes_tens = '0';
+			while (minutes_tens < '6')
+			{
+				minutes_ones = '0';
+				while (minutes_ones < 58)
+				{
+					_putchar(hours_tens);
+					_putchar(hours_ones);
+					_putchar(':');
+					_putchar(minutes_tens);
+					_putchar(minutes_ones);
+					_putchar('\n');
+					minutes_ones++;
+				}
+				minutes_ones = '0';
+				minutes_tens++;
+			}
+			minutes_tens = '0';
+			hours_ones++;
+		}
+		hours_ones = '0';
+		hours_tens++;
+	}
 }
