04c361fc1539017f85ad585c8a8500474b4c4408
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-02-21 11:19:30 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,19 +3,18 @@
 
 /**
  * print_to_98 - prints numbers to 98
- * 
  * @n: starting integer
- *
  * Description: Prints all natural numbers from n - 98
- *
  * Return: void
  */
+
 void print_to_98(int n)
 {
 	int i;
 	if (n <= 98)
+
 	{
-		for (i = n; i <=97; i++)
+		for (i = n; i <= 97; i++)
 		{
 			printf("%d, ", i);
 		}
