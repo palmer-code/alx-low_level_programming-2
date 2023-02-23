93784fb02ae7cbc63115a6a6104d097c041fc57a
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-02-23 01:11:43 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+include "main.h"
+/**
+ * _isupper - checks character is uppercase
+ * @c: carrier int variable
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c)
+{
+	if(c >= 65 && c <= 90)
+	{
+		return (i);
+	}
+	return (0);
+}
