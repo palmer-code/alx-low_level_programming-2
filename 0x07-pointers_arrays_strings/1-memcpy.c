91ed2f2f775709d12de07518c64e4c09134179e6
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-03-06 10:32:04 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *dest_start = dest;
+	unsigned int i;
 
-	while (n--)
+	for (i = 0; i < n; i++)
 	{
-		*dest++ = *src;
+		dest[i] = src[i];
 	}
-	return dest_start;
+	return (dest);
 }
