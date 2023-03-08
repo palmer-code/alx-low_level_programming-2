c55fc58226077c03b816b93bcde8cbb66de63332
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-03-07 21:52:25 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * puts_recursion - prints a string, followed by a new line
- * @s: string to be printed
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
  */
-
 void _puts_recursion(char *s)
 {
 	if (*s)
