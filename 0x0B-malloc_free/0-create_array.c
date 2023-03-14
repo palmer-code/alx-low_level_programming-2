af15bd0b46ef92794887342a0c7d7d4be9d8e4ac
Isabel_Kikuvi <isabel.kikuvi@gmail.com>
2023-03-14 07:54:42 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,19 +12,21 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *arr;
+	unsigned int i;
+
 	if (size == 0)
-	{
 		return (NULL);
-	}
-	char* arr = malloc(size * sizeof(char));
+	
+	arr = malloc(size * sizeof(char));
 
 	if (arr == NULL)
-	{
+	
 	return (NULL);
-	}
-	for (unsigned int i = 0; i < size; i++)
-	{
-	arr[i] = c;
-	}
+	
+	for (i = 0; i < size; i++)
+	
+		arr[i] = c;
+	
 	return (arr);
 }
