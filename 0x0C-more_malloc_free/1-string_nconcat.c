5d1af8bcaeb7adc5e85e2a3e733e6c8816835467
Isabel_Kikuvi <isabel.kikuvi@gmail.com>
2023-03-16 11:17:19 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= s2_len)
 		n = s2_len;
 
-	concat = malloc((s1_len + n + 1) * sizeof(char));
+	concat = malloc((s1_len + 1) * sizeof(char));
 	if (concat == NULL)
 		return (NULL);
 
