70db83788fdaa2eeb261d5f91b797ec86ae55674
Isabel_Kikuvi <isabel.kikuvi@gmail.com>
2023-03-16 23:58:59 -0700

    101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "main.h"
-#include <stdliib.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 int is_valid_number(char *num);
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]);
 
 int is_valid_number(char *num)
 {
-	while (num != '\0')
+	while (*num != '\0')
 	{
 		if (!isdigit(num))
 		{
@@ -50,14 +50,15 @@ int multiply(char *num1, char *num2)
 
 int main(int argc, char *argv[])
 {
+	char *num1, *num2;
+	int result;
+
 	{
 		if (argc != 3)
 		printf("Error\n");
 
 		return (98);
 	}
-
-	int result;
 
 	num1 = argv[1];
 	num2 = argv[2];
@@ -68,10 +69,9 @@ int main(int argc, char *argv[])
 		return (98);
 	}
 
-
 	result = multiply(num1, num2);
 	{
 		printf("%d\n", result);
-	return (0);
+		return (0);
 	}
-
+}
