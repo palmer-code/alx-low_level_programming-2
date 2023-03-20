e8f628bf7783d501ffe29d37137629af27b46f49
Isabel_Kikuvi <isabel.kikuvi@gmail.com>
2023-03-20 07:16:45 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 
+/*
+ * File: dog.h
+ * Desc: Header file that defines a new type struct dog.
+ */
+
 /**
  * struct dog - a stucture describing a dog
  * @name: name of dog
