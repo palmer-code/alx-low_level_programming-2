1c34b20b7e1b808f1ad93e4b33e7d7c8d00796ed
Isabel_Kikuvi <isabel.kikuvi@gmail.com>
2023-03-22 02:49:46 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,13 @@
 #ifndef FUNCTION_POINTER_H
 #define FUNCTION_POINTER_H
 
+/*
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0E-function_pointers directory.
+ */
+
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 
 #endif
