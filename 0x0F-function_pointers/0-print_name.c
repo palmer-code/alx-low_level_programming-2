522dd13360cfacefa3a02be7c77eb0d246cc0cf4
Isabel_Kikuvi <isabel.kikuvi@gmail.com>
2023-03-22 02:47:01 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * @print_name - A function that prints a name
+ * print_name - A function that prints a name
  * @name: name to be printed
  * @f: pointer to a function that prints a name
  */
