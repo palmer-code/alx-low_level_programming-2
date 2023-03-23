ddd6114cc5260fd8a2b78e6fe1f27b1b271ce18a
Isabel_Kikuvi <isabel.kikuvi@gmail.com>
2023-03-23 09:20:44 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,16 +14,16 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i, sum = 0;
 
 	if (n == 0)
-	{
+
 		return (0);
-	}
+
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
-	{
+
 		sum += va_arg(args, int);
-	}
+
 
 	va_end(args);
 
