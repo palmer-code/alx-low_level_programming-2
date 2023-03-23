91bf2e49ba63682790f081b5febd799165a2b91a
Isabel_Kikuvi <isabel.kikuvi@gmail.com>
2023-03-23 03:04:35 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,14 +19,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, const char *);
-		if (str == NULL)
-		{
-			printf("(nil)");
-		}
-		else
-		{
-			printf("%s", str);
-		}
+
+		printf("%s", str);
+
 
 		if (i != n - 1 && separator != NULL)
 		{
