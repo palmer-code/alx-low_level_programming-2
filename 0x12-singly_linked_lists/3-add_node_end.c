acdbbcd5670ec0d9461cba13449d389fcfc28be8
Isabel_Kikuvi <isabel.kikuvi@gmail.com>
2023-03-30 03:03:15 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -19,6 +19,11 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new_node = malloc(sizeof(list_t));
 
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
 	if (new_node->str == NULL)
 	{
 		free(new_node);
