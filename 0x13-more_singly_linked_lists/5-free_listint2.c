9d3cb445ed1ed3a7c69cdc864dccdff00233fb1f
Isabel_Kikuvi <isabel.kikuvi@gmail.com>
2023-04-04 09:53:38 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: pointer to head pointer
+ *
+ * Return: head pointer
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	temp = *head;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		free(*head);
+
+		*head = temp;
+	}
+	*head = NULL;
+}
