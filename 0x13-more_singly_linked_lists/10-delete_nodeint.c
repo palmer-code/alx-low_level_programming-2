6ee0a4ee23707aae00c1adb5f42b4692dca7324e
Isabel_Kikuvi <isabel.kikuvi@gmail.com>
2023-04-04 12:39:46 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	current = *head;
 	prev = *head;
 
-	if (*head == NULL)
+	if (current == NULL)
 		return (-1);
 
 	else if (index == 0)
